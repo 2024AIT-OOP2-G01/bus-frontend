091f18496ba1439b282e01cf2f2cf5318f17dbe7
k23002 <k23002kk@aitech.ac.jp>
2025-01-02 16:18:12 +0900

    細かい調整

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,25 @@ function App() {
         <h1>ギリギリ間に合うナビ</h1>
       </div>
       <div className="card">
+        {/*count変数用ボタン 後で消す*/}
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <fieldset id='kouzouji' className='station'>
+        <fieldset id='kouzouji' className='ele_board'>
           <h2>高蔵寺行きに間に合うまであと <span className='highlight'>{count}</span> 分</h2>
           <h2>岡崎行きに間に合うまであと <span className='highlight'>{count}</span> 分</h2>
         </fieldset>
         <br></br>
-        <fieldset id='kouzouji' className='station'>
+        <fieldset id='kouzouji' className='ele_board'>
           <h3>バスが出るまで</h3>
           <h3>あと <span className='highlight'>{count}</span> 分</h3>
         </fieldset>
         <div className='flex'>
-        <fieldset id='kouzouji' className='station'>
+        <fieldset id='kouzouji' className='ele_board'>
           <h3>高蔵寺行き</h3>
           <h3>あと <span className='highlight'>{count}</span> 分</h3>
         </fieldset>
-        <fieldset id='kouzouji' className='station'>
+        <fieldset id='kouzouji' className='ele_board'>
           <h3>岡崎行き</h3>
           <h3>あと <span className='highlight'>{count}</span> 分</h3>
         </fieldset>

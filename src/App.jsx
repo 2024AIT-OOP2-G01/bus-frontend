e5fcdd9a7f7e9f29cf00ee4a6df961d006a679c9
k23002 <k23002kk@aitech.ac.jp>
2025-01-02 16:07:02 +0900

    細かい調整

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,6 @@ function App() {
       <div className='top'>
         <h1>ギリギリ間に合うナビ</h1>
       </div>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1> */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,30 +21,24 @@ function App() {
         </fieldset>
         <br></br>
         <fieldset id='kouzouji' className='station'>
-          <h2>バスが出るまで</h2>
-          <h2>あと <span className='highlight'>{count}</span> 分</h2>
+          <h3>バスが出るまで</h3>
+          <h3>あと <span className='highlight'>{count}</span> 分</h3>
         </fieldset>
         <div className='flex'>
         <fieldset id='kouzouji' className='station'>
-          <h2>高蔵寺行き</h2>
-          <h2>あと <span className='highlight'>{count}</span> 分</h2>
+          <h3>高蔵寺行き</h3>
+          <h3>あと <span className='highlight'>{count}</span> 分</h3>
         </fieldset>
         <fieldset id='kouzouji' className='station'>
-          <h2>岡崎行き</h2>
-          <h2>あと <span className='highlight'>{count}</span> 分</h2>
+          <h3>岡崎行き</h3>
+          <h3>あと <span className='highlight'>{count}</span> 分</h3>
         </fieldset>
         </div>
         <div className='flex'>
           <button>高蔵寺行き時刻表</button>
           <button>岡崎行き時刻表</button>
         </div>
-        {/* <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p> */}
       </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   )
 }

df866e91baa0edd010654dc5c51d59437406aec9
k23002 <k23002kk@aitech.ac.jp>
2025-01-07 02:29:12 +0900

    メイン画面作成2

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,14 @@ function App() {
       <div className='top'>
         <h1>ギリギリ間に合うナビ</h1>
       </div>
+      <br></br>
+      <h4>このアプリは、愛工大のバスから愛環にスムーズに<br></br>乗車するための最短時間を表示したアプリです。</h4>
+      <p>今いる位置からバスに乗って高蔵寺行きと岡崎行きの愛環に
+スムーズに乗ることができるかどうかが表示されています。</p>
+      <fieldset className='canvas'>
       <div className="card">
-        {/*count変数用ボタン 後で消す*/}
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <fieldset id='kouzouji' className='ele_board'>
+        <fieldset id='best' className='white_board'>
+          <h4>ちょうどちょうどいいバスは…</h4>
           <h2>高蔵寺行きに間に合うまであと <span className='highlight'>{count}</span> 分</h2>
           <h2>岡崎行きに間に合うまであと <span className='highlight'>{count}</span> 分</h2>
         </fieldset>
@@ -35,11 +37,19 @@ function App() {
           <h3>あと <span className='highlight'>{count}</span> 分</h3>
         </fieldset>
         </div>
+        <br></br>
+        <fieldset id='next' className='white_board'>
+          <h4>次にちょうどいいバスは…</h4>
+          <h2>高蔵寺行きに間に合うまであと <span className='highlight'>{count}</span> 分</h2>
+          <h2>岡崎行きに間に合うまであと <span className='highlight'>{count}</span> 分</h2>
+        </fieldset>
         <div className='flex'>
           <button>高蔵寺行き時刻表</button>
           <button>岡崎行き時刻表</button>
         </div>
+        <ul id="timetable"></ul>
       </div>
+      </fieldset>
     </>
   )
 }

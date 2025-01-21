2f121b8432aa44eca5217a638a0617216d52069f
tokotaya02 <k.ktoo472@gmail.com>
2025-01-21 15:39:02 +0900

    css調整

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import styled from "styled-components";
+
+const H2 = styled.h2`
+  font-size: 20px;
+`
 
 const Title = () => {
   return (
     <div>
       <h1>ギリギリ間に合うナビ</h1>
       <br />
-      <h2>このアプリは、愛工大のバスから愛環にスムーズに<br></br>乗車するための最短時間を表示したアプリです。</h2>
+      <H2>このアプリは、愛工大のバスから愛環にスムーズに<br></br>乗車するための最短時間を表示したアプリです。</H2>
     </div>
   )
 }
